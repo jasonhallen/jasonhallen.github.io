62aabafa28f9164254f0a7b3b936c661db5b282f
Jason Hallen <hallenj@Jasons-MacBook-Pro.local>
2024-06-23 15:04:39 -0500

    Updates

--- videos.js
+++ videos.js
@@ -39,9 +39,194 @@ var videos_available = videos_all.map((x) => x)
 videos_available.splice(videos_available.indexOf(starting_video), 1)
 
 var videos_blocked = []
-var active_timer
+var video_timer
 var active_video_list = []
 var video_playing = false
+
+class HomeVideo {
+    constructor(id) {
+        console.log("Initializing HomeVideo instance")
+        this.element = document.getElementById(id)
+        console.log(this.element)
+
+        // this.element.onloadedmetadata = this.set_video_start_duration.bind(this)
+        // this.element.onseeked = this.add_video_to_dom.bind(this)
+        
+        this.duration
+        this.start_time
+        this.timer
+
+        this.element.onloadedmetadata = this.set_video_start_duration.bind(this)
+    }
+
+    set_video_start_duration() {
+        // var time_left = this.duration - this.currentTime
+        // console.log(this.duration, this.currentTime)
+        console.log(`starting set_video_start_duration: ${this}`)
+        if (this.element.duration > 4) {
+            this.duration = Math.random() * 3 + 0.5
+            this.start_time = Math.random() * (this.element.duration - this.duration)
+        // determine if video is playing
+        // if (home_video_list.length && !home_video_list[0].paused) {
+        //     var start_time = set_time_duration(this).toString()
+        } else {
+            this.duration = 0
+            this.start_time = "0.0"
+        }
+        this.element.currentTime = 4.0//this.start_time.toString()
+        this.element.onseeked = swap_video_elements.bind(this)
+        // this.ended = 0
+        console.log(`total = ${this.element.duration}, current_time = ${this.element.currentTime}, start_time = ${this.start_time}, duration = ${this.duration}, autoplay = ${this.element.autoplay}, source = ${this.element.src}`)
+        // this.ontimeupdate = check_elapsed_time(this, start_time, duration)
+    }    
+}
+
+var home_video_0 = new HomeVideo('vid0')
+var home_video_1 = new HomeVideo('vid1')
+var home_video_list = [home_video_0, home_video_1]
+
+function change_video(change_button_clicked = false) {
+    console.log(`CHANGE_VIDEO: change_button_clicked = ${change_button_clicked}`)
+    // var video_element = document.querySelector('.home_video')
+    var home_video = home_video_list[0]
+    console.log(home_video)
+    home_video.element.src = '/video/' + select_video_source()
+    // home_video.element.onloadedmetadata = home_video.set_video_start_duration.bind(home_video)
+}
+
+function select_video_source() {
+    var video_source = videos_available[Math.floor(Math.random() * videos_available.length)]
+    videos_available.splice(videos_available.indexOf(video_source), 1)
+    videos_blocked.push(video_source)
+    if (videos_blocked.length > 15) {
+        videos_available.push(videos_blocked.shift())
+    }
+    return video_source
+}
+
+function set_video_start_duration() {
+    // var time_left = this.duration - this.currentTime
+    // console.log(this.duration, this.currentTime)
+    console.log(`starting set_video_start_duration: ${this}, ${test}`)
+    if (this.element.duration > 4) {
+        this.duration = Math.random() * 3 + 0.5
+        this.start_time = Math.random() * (this.element.duration - this.duration)
+    // determine if video is playing
+    // if (home_video_list.length && !home_video_list[0].paused) {
+    //     var start_time = set_time_duration(this).toString()
+    } else {
+        this.duration = 0
+        this.start_time = "0.0"
+    }
+    this.element.currentTime = "4.0"//this.start_time.toString()
+    this.element.onseeked = swap_video_elements.bind(this)
+    // this.ended = 0
+    console.log(`total = ${this.element.duration}, current_time = ${this.element.currentTime}, start_time = ${this.start_time}, duration = ${this.duration}, autoplay = ${this.element.autoplay}, source = ${this.element.src}`)
+    // this.ontimeupdate = check_elapsed_time(this, start_time, duration)
+}
+
+function swap_video_elements() {
+    console.log(`swapping video elements: ${this.element.currentTime}, start_time = ${this.start_time}, duration = ${this.duration}`)
+
+    var home_video_container = document.getElementById('home_video_container')
+    // var home_video_list = document.getElementsByClassName('home_video')
+    if (video_playing == true) {
+        this.element.autoplay = true
+        this.element.play()
+        // start timer
+        clearInterval(this.timer)
+        this.timer = setInterval(check_elapsed_time.bind(this))
+    } else {
+        home_video_list[1].element.pause()
+    }
+    
+    home_video_container.insertBefore(home_video_list[1].element, home_video_list[0].element)
+    home_video_list.reverse()
+
+    // document.querySelector('.home_video').pause()
+    // document.querySelector('.home_video').src = ''
+    home_video_list[0].element.pause()
+    home_video_list[0].element.src = ''
+    
+    // if (home_video_list.length && !home_video_list[0].paused) {
+    //     // this.element.play()
+    //     this.timer = setInterval(this.check_elapsed_time.bind(this), 30)
+    // }
+    
+    // if (this.element.autoplay) {
+    //     this.timer = setInterval(this.check_elapsed_time.bind(this), 30)
+    // }
+    update_button_position()
+}
+
+function check_elapsed_time() {
+    console.log("check_elapsed_time")
+    var time_elapsed = this.element.currentTime - this.start_time
+        // console.log(`playing: time_elapsed = ${time_elapsed}, start_time = ${this.start_time}, duration = ${this.duration}`)
+        if (time_elapsed >= this.duration) {
+            console.log(`reached end of duration`)
+            // this.ended += 1
+            clearInterval(this.timer)
+            change_video()
+        }
+}
+
+function new_toggle_play() {
+    // var active_video = document.getElementsByClassName('home_video')[0]
+    var play_pause_button = document.getElementById('play_pause')
+
+    // if (active_video_list.some((video) => video.element.paused == false)) {
+    if (video_playing == true) {
+        console.log(`PLAY_PAUSE: video_playing = ${video_playing}, pausing`)
+        play_pause_button.innerHTML = ">"
+        play_pause_button.classList.remove('rotate')
+        // active_video_list.forEach(video => {
+        //     // console.log(`playing = ${!video.element.paused}`)
+        //     video.element.pause()
+        //     video.element.autoplay = false
+        //     clearInterval(video.timer)
+        // })
+        clearInterval(home_video_list[1].timer)
+        document.getElementsByClassName('home_video')[1].pause()
+        video_playing = false
+        // active_video.pause()
+        // clearTimeout(active_timer)
+    } else {
+        console.log(`PLAY_PAUSE: video_playing = ${video_playing}, starting`)
+        play_pause_button.innerHTML = "="
+        play_pause_button.classList.add('rotate')
+        // set_time_duration(active_video)
+        // active_video_list.forEach(video => {
+        //     // console.log(`playing = ${!video.element.paused}`)
+        //     video.element.play()
+        //     video.timer = setInterval(video.check_elapsed_time.bind(video), 30)
+        // })
+        // video_timer = setInterval(check_elapsed_time.bind(document.getElementsByClassName('home_video')[1], start_time, duration))
+        document.getElementsByClassName('home_video')[1].play()
+        home_video_list[1].timer = setInterval(check_elapsed_time.bind(home_video_list[1]))
+        home_video_list[1].element.play()
+        video_playing = true
+        // active_video.play()        
+    }
+}
+
+window.onresize = update_button_position
+change_video()
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
 
 class CustomVideo {
     constructor(button_clicked) {
@@ -73,24 +258,24 @@ class CustomVideo {
         console.log('Finished initialization')
     }
 
-    set_video_start_duration() {
-        // var time_left = this.duration - this.currentTime
-        // console.log(this.duration, this.currentTime)
-        console.log("Starting set_video_start_duration")
-        if (this.element.duration > 4) {
-            this.duration = Math.random() * 3 + 0.5
-            this.start_time = Math.random() * (this.element.duration - this.duration)
-        // determine if video is playing
-        // if (home_video_list.length && !home_video_list[0].paused) {
-        //     var start_time = set_time_duration(this).toString()
-        } else {
-            this.start_time = "0.0"
-        }
-        this.element.currentTime = this.start_time
-        // this.ended = 0
-        console.log(`total = ${this.element.duration}, start_time = ${this.start_time}, duration = ${this.duration}, autoplay = ${this.element.autoplay}, source = ${this.element.src}`)
-        // this.ontimeupdate = check_elapsed_time(this, start_time, duration)
-    }
+    // set_video_start_duration() {
+    //     // var time_left = this.duration - this.currentTime
+    //     // console.log(this.duration, this.currentTime)
+    //     console.log(`Starting set_video_start_duration: ${this}`)
+    //     if (this.element.duration > 4) {
+    //         this.duration = Math.random() * 3 + 0.5
+    //         this.start_time = Math.random() * (this.element.duration - this.duration)
+    //     // determine if video is playing
+    //     // if (home_video_list.length && !home_video_list[0].paused) {
+    //     //     var start_time = set_time_duration(this).toString()
+    //     } else {
+    //         this.start_time = "0.0"
+    //     }
+    //     this.element.currentTime = this.start_time
+    //     // this.ended = 0
+    //     console.log(`total = ${this.element.duration}, start_time = ${this.start_time}, duration = ${this.duration}, autoplay = ${this.element.autoplay}, source = ${this.element.src}`)
+    //     // this.ontimeupdate = check_elapsed_time(this, start_time, duration)
+    // }
 
     add_video_to_dom() {
         console.log('adding to DOM')
@@ -142,8 +327,6 @@ class CustomVideo {
             create_new_video()
         }
     }
-
-
 }
 
 function update_button_position() {
@@ -243,16 +426,6 @@ function toggle_play() {
 //     update_button_position()
 // }
 
-function select_video_source() {
-    var video_source = videos_available[Math.floor(Math.random() * videos_available.length)]
-    videos_available.splice(videos_available.indexOf(video_source), 1)
-    videos_blocked.push(video_source)
-    if (videos_blocked.length > 15) {
-        videos_available.push(videos_blocked.shift())
-    }
-    return video_source
-}
-
 function create_new_video(button_clicked = false) {
     console.log("CREATE_NEW_VIDEO: button_clicked = " + button_clicked)
     // var current_video = document.getElementsByClassName('home_video')[0]
@@ -281,5 +454,3 @@ function create_new_video(button_clicked = false) {
     // new_video.onloadedmetadata = set_video_start_duration
     // new_video.onseeked = add_video_to_dom
 }
-
-window.onresize = update_button_position
